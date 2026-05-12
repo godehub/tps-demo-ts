fcee7b81d6b85541de37de121f6fd02242bfd502
moluopro <moluopro@qq.com>
2026-05-12 18:54:48 +0800

    Convert camera shake script to TypeScript

--- player/camera_noise_shake_effect.ts
+++ player/camera_noise_shake_effect.ts
@@ -1,5 +1,6 @@
 import godot from "godot";
 const { Camera3D, FastNoiseLite, Vector3 } = godot;
+import type { FastNoiseLite as FastNoiseLiteType, Vector3 as Vector3Type } from "godot";
 
 const SPEED = 1.0;
 const DECAY_RATE = 1.5;
@@ -8,36 +9,37 @@ const MAX_PITCH = 0.05;
 const MAX_ROLL = 0.1;
 const MAX_TRAUMA = 1.2;
 
-class CameraNoiseShakeEffect extends Camera3D {
-	start_rotation = new Vector3();
+export default class CameraNoiseShakeEffect extends Camera3D {
+	start_rotation: Vector3Type = new Vector3();
 	trauma = 0.0;
 	time = 0.0;
-	noise = new FastNoiseLite();
+	noise: FastNoiseLiteType = new FastNoiseLite();
 	noise_seed = Math.floor(Math.random() * 2147483647);
 
-	_ready() {
+	_ready(): void {
+		this.physics_interpolation_mode = 2;
 		this.noise.seed = this.noise_seed;
 		this.noise.fractal_octaves = 1;
 		this.noise.fractal_lacunarity = 1.0;
 		this.start_rotation = new Vector3(this.rotation);
 	}
 
-	_process(delta) {
+	_process(delta: number): void {
 		if (this.trauma > 0.0) {
 			this.decay_trauma(delta);
 			this.apply_shake(delta);
 		}
 	}
 
-	add_trauma(amount) {
+	add_trauma(amount: number): void {
 		this.trauma = Math.min(this.trauma + amount, MAX_TRAUMA);
 	}
 
-	decay_trauma(delta) {
+	decay_trauma(delta: number): void {
 		this.trauma = Math.max(this.trauma - DECAY_RATE * delta, 0.0);
 	}
 
-	apply_shake(delta) {
+	apply_shake(delta: number): void {
 		this.time += delta * SPEED * 5000.0;
 		const shake = this.trauma * this.trauma;
 		const yaw = MAX_YAW * shake * this.get_noise_value(this.noise_seed, this.time);
@@ -50,11 +52,8 @@ class CameraNoiseShakeEffect extends Camera3D {
 		);
 	}
 
-	get_noise_value(seedValue, pos) {
+	get_noise_value(seedValue: number, pos: number): number {
 		this.noise.seed = seedValue;
 		return this.noise.get_noise_1d(pos);
 	}
 }
-
-
-export default CameraNoiseShakeEffect;
