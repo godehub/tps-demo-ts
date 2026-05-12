7c24a4ae07b0d23019d59649e6a12c6ae77c598b
moluopro <moluopro@qq.com>
2026-05-12 18:54:48 +0800

    Convert player input script to TypeScript

--- player/player_input.ts
+++ player/player_input.ts
@@ -1,5 +1,6 @@
 import godot from "godot";
 const { InputEventMouseMotion, MultiplayerSynchronizer, PhysicsRayQueryParameters3D, Vector2, Vector3 } = godot;
+import type { AnimationPlayer, Camera3D, ColorRect, Control, InputEvent, Node3D, Vector2 as Vector2Type, Vector3 as Vector3Type } from "godot";
 
 const Input = globalThis.Input;
 
@@ -9,7 +10,7 @@ const CAMERA_X_ROT_MIN = -89.9 * Math.PI / 180.0;
 const CAMERA_X_ROT_MAX = 70.0 * Math.PI / 180.0;
 const AIM_HOLD_THRESHOLD = 0.4;
 
-class PlayerInputSynchronizer extends MultiplayerSynchronizer {
+export default class PlayerInputSynchronizer extends MultiplayerSynchronizer {
 	static exports = {
 		aiming: { type: "bool" },
 		shoot_target: { type: "Vector3" },
@@ -26,17 +27,27 @@ class PlayerInputSynchronizer extends MultiplayerSynchronizer {
 	toggled_aim = false;
 	aiming_timer = 0.0;
 	aiming = false;
-	shoot_target = new Vector3();
-	motion = new Vector2();
+	shoot_target: Vector3Type = new Vector3();
+	motion: Vector2Type = new Vector2();
 	shooting = false;
 	jumping = false;
+	declare player: PlayerNode;
+	declare space_state: PhysicsSpaceState;
+	ray_exclude: unknown[] = [];
+	declare camera_animation: AnimationPlayer;
+	declare crosshair: Control;
+	declare camera_base: Node3D;
+	declare camera_rot: Node3D;
+	declare camera_camera: Camera3D;
+	declare color_rect: ColorRect;
 
-	_ready() {
+	_ready(): void {
 		// This synchronizer samples local input, then replicates the exported fields to the player owner.
-		this.player = this.get_parent();
+		this.player = this.get_parent() as PlayerNode;
 		this.space_state = this.player.get_world_3d().direct_space_state;
 		this.ray_exclude = [this];
 		if (this.get_multiplayer_authority() === this.get_multiplayer().get_unique_id()) {
+			this.disable_camera_physics_interpolation();
 			this.camera_camera.make_current();
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
 		} else {
@@ -46,7 +57,15 @@ class PlayerInputSynchronizer extends MultiplayerSynchronizer {
 		}
 	}
 
-	_process(delta) {
+	disable_camera_physics_interpolation(): void {
+		const off = 2;
+		this.camera_base.physics_interpolation_mode = off;
+		this.camera_rot.physics_interpolation_mode = off;
+		this.camera_camera.get_parent().physics_interpolation_mode = off;
+		this.camera_camera.physics_interpolation_mode = off;
+	}
+
+	_process(delta: number): void {
 		this.motion = new Vector2(
 			Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 			Input.get_action_strength("move_back") - Input.get_action_strength("move_forward"),
@@ -97,9 +116,9 @@ class PlayerInputSynchronizer extends MultiplayerSynchronizer {
 				rayFrom.y + rayDir.y * 1000.0,
 				rayFrom.z + rayDir.z * 1000.0,
 			);
-			const query = PhysicsRayQueryParameters3D.create(rayFrom, rayTo, 0b11, this.ray_exclude);
-			const col = this.space_state.intersect_ray(query);
-			this.shoot_target = Object.keys(col).length === 0 ? rayTo : col.position;
+			const query = PhysicsRayQueryParameters3D.create(rayFrom, rayTo, 0b11, this.ray_exclude as any);
+			const col = this.space_state.intersect_ray(query) as Partial<RayHit>;
+			this.shoot_target = Object.keys(col).length === 0 ? rayTo : col.position ?? rayTo;
 		}
 
 		const playerTransform = this.player.global_transform;
@@ -112,7 +131,7 @@ class PlayerInputSynchronizer extends MultiplayerSynchronizer {
 		this.color_rect.modulate = modulate;
 	}
 
-	_input(input_event) {
+	_input(input_event: InputEvent): void {
 		if (input_event instanceof InputEventMouseMotion) {
 			let cameraSpeedThisFrame = CAMERA_MOUSE_ROTATION_SPEED;
 			if (this.aiming) {
@@ -125,7 +144,7 @@ class PlayerInputSynchronizer extends MultiplayerSynchronizer {
 		}
 	}
 
-	rotate_camera(move) {
+	rotate_camera(move: Vector2Type): void {
 		this.camera_base.rotate_y(-move.x);
 		this.camera_base.orthonormalize();
 		const rotation = this.camera_rot.rotation;
@@ -133,23 +152,20 @@ class PlayerInputSynchronizer extends MultiplayerSynchronizer {
 		this.camera_rot.rotation = rotation;
 	}
 
-	get_aim_rotation() {
+	get_aim_rotation(): number {
 		const cameraXRot = Math.max(CAMERA_X_ROT_MIN, Math.min(this.camera_rot.rotation.x, CAMERA_X_ROT_MAX));
 		return cameraXRot >= 0.0 ? -cameraXRot / CAMERA_X_ROT_MAX : cameraXRot / CAMERA_X_ROT_MIN;
 	}
 
-	get_camera_base_quaternion() {
+	get_camera_base_quaternion(): import("godot").Quaternion {
 		return this.camera_base.global_transform.basis.get_rotation_quaternion();
 	}
 
-	get_camera_rotation_basis() {
+	get_camera_rotation_basis(): import("godot").Basis {
 		return this.camera_rot.global_transform.basis;
 	}
 
-	jump() {
+	jump(): void {
 		this.jumping = true;
 	}
 }
-
-
-export default PlayerInputSynchronizer;
